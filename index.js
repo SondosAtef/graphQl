14116cd05d59d0546c9add6537744332e71606d1
Sondos Atef <sondos.ahmed031@gmail.com>
2022-05-08 12:35:56 +0200

    fixed posts

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
 const {ApolloServer ,gql} = require("apollo-server");
 
+const posts=[
+    {
+        title:"post",
+        id : "123"
+    },
+    {
+        title: "post 2",
+        id : "321"
+    }
+]
+
 //schema
 const typeDefs= gql`
+type Post{
+    id:String,
+    title:String
+}
+
 type Query{
-  posts:[String]
+  posts:[Post]
 }
 `;
 
 
 // resolver
 const resolvers = {
-    query:{
-        posts:()=>{
-         const posts =  [ { id: "1", title: "mohamed" },
-                        { id: "2", title: "ahmed" },
-                         ];
-      return posts;
-        }
+    Query:{
+        posts:()=> posts
     }
 }
 
 
 //server
 const server = new ApolloServer({
-    typeDefs
+    typeDefs,
+    resolvers
 })
 
 server.listen(7080 , ()=>{
